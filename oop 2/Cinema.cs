67bfa22bb8d63a04463416590382f84b2b2bc1c0
Kaytbay <mohamedkaytbay@gmail.com>
2026-02-27 16:55:47 +0200

    The Cinema Class

---
+++ oop 2/Cinema.cs
@@ -0,0 +1,68 @@
+﻿using oop_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2
+{
+    internal class Cinema
+    {
+
+        private Ticket[] _tickets = new Ticket[20];
+
+
+        public Ticket this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index <20)
+                {
+                    return _tickets[index];
+                }
+                return null;
+            }
+            set
+            {
+                if (index >= 0 && index < 20)
+                {
+                    _tickets[index] = value;
+                }
+            }
+        }
+
+
+        public Ticket this[string name]
+        {
+            get
+            {
+                foreach (var movie in _tickets)
+                {
+                    if (movie.MovieName == name)
+                    {
+                        return movie;
+                    }
+                  
+                }
+                return null;
+            }
+        }
+
+
+        public bool AddTicket(Ticket t)
+        {
+            for (int i = 0; i < _tickets.Length; i++)
+            {
+                if (_tickets[i] == null)
+                {
+                    _tickets[i] = t;
+                   return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
